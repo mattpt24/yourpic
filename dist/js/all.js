47dbb8383318909dbb9161f6e2b854e1edf67b6f
unknown <mattypt@hotmail.co.uk>
2022-08-02 17:24:15 +0200

    feat: Added classes section and made responsive!

--- dist/js/all.js
+++ dist/js/all.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("Connected!");
+var navbar = document.querySelector("#navbar");
+window.addEventListener("scroll", change);
+
+function change() {
+  var topOfPage = window.scrollY;
+
+  if (topOfPage >= 50) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
+}
