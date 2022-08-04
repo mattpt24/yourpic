b766a04c86c8f46f94216cdf978e2eb7e6d59c42
unknown <mattypt@hotmail.co.uk>
2022-08-05 01:02:08 +0200

    Adding build folder into the respository

---
+++ build/js/script.js
@@ -0,0 +1,16 @@
+const navbar = document.querySelector("#navbar");
+
+window.addEventListener("scroll", change);
+
+function change() {
+    var topOfPage = window.scrollY;
+    
+    if(topOfPage >= 50) {
+        navbar.classList.add("fixed-nav");
+    }
+
+    
+    else {
+        navbar.classList.remove("fixed-nav");
+    }
+}
