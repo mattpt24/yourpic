451ad4296df10274218e68bde5af74ebcd245417
unknown <mattypt@hotmail.co.uk>
2022-08-09 15:38:55 +0200

    feat: Added dropdown navbar menu, tested

--- build/js/script.js
+++ build/js/script.js
@@ -1,4 +1,14 @@
 const navbar = document.querySelector("#navbar");
+const openDropdownBtn = document.querySelector(".burger__icon__container");
+const dropdownNavbar = document.querySelector(".dropdown__navbar__container");
+const burgerLine1 = document.querySelector(".line1");
+const burgerLine2 = document.querySelector(".line2");
+const burgerLine3 = document.querySelector(".line3");
+
+
+
+
+
 
 window.addEventListener("scroll", change);
 
@@ -14,3 +24,34 @@ function change() {
         navbar.classList.remove("fixed-nav");
     }
 }
+
+
+
+// DROPDOWN NAVBAR
+openDropdownBtn.addEventListener("click", () => {
+    dropdownNavbar.classList.toggle("open--dropdown--navbar");
+});
+
+
+openDropdownBtn.addEventListener("click", () => {
+   burgerLine1.classList.toggle("rotate1");
+   burgerLine2.classList.toggle("hidden");
+   burgerLine3.classList.toggle("rotate2");
+ });
+
+
+
+ const dropdownNavLinks = document.querySelectorAll(".dropdown__nav__link");
+const dropdownNavLinksArray = Array.from(dropdownNavLinks);
+
+
+ for(let i = 0; i <= dropdownNavLinksArray.length; i++) {
+    dropdownNavLinksArray[i].addEventListener("click", () => {
+        dropdownNavbar.classList.toggle("open--dropdown--navbar");
+        burgerLine1.classList.remove("rotate1");
+        burgerLine2.classList.remove("hidden");
+        burgerLine3.classList.remove("rotate2");
+    })
+ }
+
+ console.log(dropdownNavLinksArray);
--- dist/js/all.js
+++ dist/js/all.js
@@ -1,6 +1,11 @@
 "use strict";
 
 var navbar = document.querySelector("#navbar");
+var openDropdownBtn = document.querySelector(".burger__icon__container");
+var dropdownNavbar = document.querySelector(".dropdown__navbar__container");
+var burgerLine1 = document.querySelector(".line1");
+var burgerLine2 = document.querySelector(".line2");
+var burgerLine3 = document.querySelector(".line3");
 window.addEventListener("scroll", change);
 
 function change() {
@@ -11,4 +16,27 @@ function change() {
   } else {
     navbar.classList.remove("fixed-nav");
   }
+} // DROPDOWN NAVBAR
+
+
+openDropdownBtn.addEventListener("click", function () {
+  dropdownNavbar.classList.toggle("open--dropdown--navbar");
+});
+openDropdownBtn.addEventListener("click", function () {
+  burgerLine1.classList.toggle("rotate1");
+  burgerLine2.classList.toggle("hidden");
+  burgerLine3.classList.toggle("rotate2");
+});
+var dropdownNavLinks = document.querySelectorAll(".dropdown__nav__link");
+var dropdownNavLinksArray = Array.from(dropdownNavLinks);
+
+for (var i = 0; i <= dropdownNavLinksArray.length; i++) {
+  dropdownNavLinksArray[i].addEventListener("click", function () {
+    dropdownNavbar.classList.toggle("open--dropdown--navbar");
+    burgerLine1.classList.remove("rotate1");
+    burgerLine2.classList.remove("hidden");
+    burgerLine3.classList.remove("rotate2");
+  });
 }
+
+console.log(dropdownNavLinksArray);
